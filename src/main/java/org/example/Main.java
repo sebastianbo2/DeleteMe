487acfd83953956a5311cac9dfb82e61f343d60e
Sebastian Bobos <6305020@edu.vaniercollege.qc.ca>
2025-04-29 14:41:13 -0400

    Add bubbleSort method in Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] nums = {3, 1, 5, 2, 1, 4};
+        int[] numsBackup = Arrays.copyOf(nums, nums.length);
+        bubbleSort(nums);
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+    /**
+     * sorts an array by bringing the largest value in each iteration towards the end of the array
+     * @param nums the input array
+     */
+    public static void bubbleSort(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = 0; j < nums.length - 1 - i; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
     }
 }
